01fe8daa758599329541b28da0fe9e3702a483a3
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-07-06 23:22:50 +0200

    Session destructor

--- glonax/client.py
+++ glonax/client.py
@@ -200,6 +200,12 @@ class Session:
         self.writer.writer.close()
         await self.writer.writer.wait_closed()
 
+    def __del__(self):
+        """
+        Destructor for the Session class.
+        """
+        self.close()
+
     async def __aenter__(self):
         return self
 
