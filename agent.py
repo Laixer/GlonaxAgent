0fdb4da4b2060a388ae1cfd94b20a7c684537415
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-07-15 20:12:47 +0200

    Refactor ColorFormatter class in agent.py for improved logging formatting

--- agent.py
+++ agent.py
@@ -32,16 +32,16 @@ logger = logging.getLogger()
 
 class ColorFormatter(logging.Formatter):
     COLORS = {
-        "DEBUG": "\033[38;21m",
-        "INFO": "\033[32m",
-        "WARNING": "\033[33;1m",
-        "ERROR": "\033[31;1m",
-        "CRITICAL": "\033[31;1m",
+        logging.DEBUG: "\033[38;21m",
+        logging.INFO: "\033[32m",
+        logging.WARNING: "\033[33;1m",
+        logging.ERROR: "\033[31;1m",
+        logging.CRITICAL: "\033[31;1m",
     }
     RESET = "\033[0m"
 
-    def format(self, record):
-        log_color = self.COLORS.get(record.levelname, self.RESET)
+    def format(self, record: logging.LogRecord):
+        log_color = self.COLORS.get(record.levelno, self.RESET)
         log_fmt = f"%(asctime)s | {log_color}%(levelname)8s{self.RESET} | %(message)s"
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
